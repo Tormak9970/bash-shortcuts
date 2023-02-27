3457a1cfb6600ac7a03c87f95e6554c3c1439b06
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-02-27 17:01:56 -0500

    need to add wsproto

--- jsInterop.py
+++ jsInterop.py
@@ -1,25 +1,30 @@
 from subprocess import Popen
 from logger import log
+from webSocketClient import Client as WebSocketClient
+import json
 
 class JsInteropManager:
   def __init__(self, hostName, port):
     self.hostName = hostName
     self.port = port
-    
+    self.client = WebSocketClient(f"ws://{self.hostname}:{self.port}/")
     pass
 
   def startServer(self):
     log(f"Starting Websocket server on port {self.port}")
-    self.serverProcess = Popen(["python", "./webSocketServer.py", self.hostName, self.port])
+    self.serverProcess = Popen(["python", "./server.py", self.hostName, self.port])
+    self.client.handshake()
     pass
 
-  def sendMessage(self):
-    # self.server
+  def sendMessage(self, message: str, data: str):
+    self.client.send(json.dumps({ "message": message, "data": data }))
     pass
 
   def stopServer(self):
     log(f"Killing Websocket server on port {self.port}")
     self.serverProcess.kill()
+    log(f"Killing Websocket client")
+    self.client.close()
     pass
 
         
---
+++ server.py
@@ -0,0 +1,33 @@
+from webSocketServer import WebSocketServer, WebSocket
+import sys
+
+class InteropServer(WebSocket):
+  def handle(self):
+    for client in clients:
+      if client != self:
+        client.send_message(self.address[0] + u' - ' + self.data)
+
+  def connected(self):
+    print(self.address, 'connected')
+    for client in clients:
+      client.send_message(self.address[0] + u' - connected')
+    clients.append(self)
+
+  def handle_close(self):
+    clients.remove(self)
+    print(self.address, 'closed')
+    for client in clients:
+      client.send_message(self.address[0] + u' - disconnected')
+
+
+clients = []
+
+args = sys.argv
+numArgs = len(args)
+
+if (len(args) >= 3):
+  server = WebSocketServer(args[1], args[2], InteropServer)
+  server.serve_forever()
+else:
+  Exception(f"Expected two arguments but only found {len(sys.argv)}")
+      
