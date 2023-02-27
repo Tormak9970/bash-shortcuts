f549da182c43c7409a8f993a7ee64f4e4fc61be2
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-02-27 10:29:53 -0600

    working on fixing new settings

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import json
 import os
 import sys
-from copy import deepcopy
 
 sys.path.append(os.path.dirname(__file__))
 
@@ -25,23 +24,23 @@ class Plugin:
 
   # Normal methods: can be called from JavaScript using call_plugin_function("signature", argument)
   async def getShortcuts(self):
-    return self.settingsManager.settings.shortcuts
+    return self.settingsManager.getSetting("shortcuts", {})
       
   async def addShortcut(self, shortcut):
     self._addShortcut(self, shortcut)
-    return self.settingsManager.settings.shortcuts
+    return self.settingsManager.getSetting("shortcuts", {})
 
   async def setShortcuts(self, shortcuts):
     self._setShortcuts(self, shortcuts)
-    return self.settingsManager.settings.shortcuts
+    return self.settingsManager.getSetting("shortcuts", {})
 
   async def modShortcut(self, shortcut):
     self._modShortcut(self, shortcut)
-    return self.settingsManager.settings.shortcuts
+    return self.settingsManager.getSetting("shortcuts", {})
 
   async def remShortcut(self, shortcut):
     self._remShortcut(self, shortcut)
-    return self.settingsManager.settings.shortcuts
+    return self.settingsManager.getSetting("shortcuts", {})
 
   async def runNonAppShortcut(self, shortcutId):
     self._runNonAppShortcut(self, shortcutId)
@@ -68,15 +67,19 @@ class Plugin:
     self.settingsManager.read()
     
     if "shortcuts" not in self.settingsManager.settings:
+      log("No shortcuts detected in settings.")
       if (os.path.exists(self.oldShortcutsPath)):
+        log("Converting old shortcuts.")
         try:
           with open(self.oldShortcutsPath, "r") as file:
             shortcutsDict = json.load(file)
+            log(f"Got shortcuts from old shortcuts.json. Shortcuts: {json.dumps(shortcutsDict)}")
             self.settingsManager.setSetting("shortcuts", shortcutsDict)
             
         except Exception as e:
           log(f"Exception while parsing shortcuts: {e}") # error reading json
       else:
+        log("Adding default shortcut.")
         self.settingsManager.setSetting("shortcuts", { "fcba1cb4-4601-45d8-b919-515d152c56ef": { "id": "fcba1cb4-4601-45d8-b919-515d152c56ef", "name": "Konsole", "cmd": "konsole", "position": 1, "isApp": True } })
 
     self.instanceManager.listenForThreadEvents()
@@ -90,8 +93,10 @@ class Plugin:
   def _addShortcut(self, shortcut):
     if (shortcut["id"] not in self.shortcuts):
       log(f"Adding shortcut {shortcut['name']}")
-      self.settingsManager.settings.shortcuts[shortcut["id"]] = shortcut
-      self.settingsManager.commit()
+      shortcutsDict = self.settingsManager.getSetting("shortcuts", {})
+      shortcutsDict[shortcut["id"]] = shortcut
+
+      self.settingsManager.setSetting("shortcuts", shortcutsDict)
     else:
       log(f"Shortcut {shortcut['name']} already exists")
 
@@ -106,8 +111,10 @@ class Plugin:
   def _modShortcut(self, shortcut):
     if (shortcut["id"] in self.shortcuts):
       log(f"Modifying shortcut {shortcut['name']}")
-      self.settingsManager.settings.shortcuts[shortcut["id"]] = shortcut
-      self.settingsManager.commit()
+      shortcutsDict = self.settingsManager.getSetting("shortcuts", {})
+      shortcutsDict[shortcut["id"]] = shortcut
+      
+      self.settingsManager.setSetting("shortcuts", shortcutsDict)
     else:
       log(f"Shortcut {shortcut['name']} does not exist")
 
@@ -116,8 +123,10 @@ class Plugin:
   def _remShortcut(self, shortcut):
     if (shortcut["id"] in self.shortcuts):
       log(f"Removing shortcut {shortcut['name']}")
-      del self.settingsManager.settings.shortcuts[shortcut["id"]]
-      self.settingsManager.commit()
+      shortcutsDict = self.settingsManager.getSetting("shortcuts", {})
+      del shortcutsDict[shortcut["id"]]
+      
+      self.settingsManager.setSetting("shortcuts", shortcutsDict)
     else:
       log(f"Shortcut {shortcut['name']} does not exist")
 
