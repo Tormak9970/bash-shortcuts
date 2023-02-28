b93401ad352c9aa7a5a9d59f898260deb70cdd14
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-02-28 14:14:31 -0600

    checked deploy script is working

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 
-deck_ip=""
-deck_port=""
-deck_pass=""
-deck_ssh_key=""
-deck_home_dir=""
-
-echo "[INFO]: Deploying plugin to deck..."
-echo ""
 echo "[TASK]: Loading config..."
 
-unclean_output=$(cat ./.vscode/settings.json)
-echo $unclean_output
-prepped_output="${unclean_output//[\s\{\}\"]/""}"
-echo $prepped_output
+unclean_output=$(<.vscode/settings.json)
+prepped_output="${unclean_output//[\s\{\}\" ]/""}"
+
+IFS=',:' read -r -a tmps <<< $prepped_output
+
+deck_ip="${tmps[1]}"
+deck_port="${tmps[3]}"
+deck_home_dir="${tmps[5]}/Desktop/dev-plugins/Shortcuts"
+
+echo "[INFO]: Loaded config"
+echo ""
+echo "[TASK]: Deploying plugin to deck..."
+
+# -r flag to copy a directory
+# format: scp -P $deck_port SRC_PATH deck@$deck_ip:DEST_PATH
+
+#? Copy general files
+echo "[TASK]: Copying general files..."
+genFiles=(LICENSE main.py package.json plugin.json README.md)
+
+for genFile in "${genFiles[@]}"; do
+  scp -P $deck_port $genFile deck@$deck_ip:$deck_home_dir/$genFile
+  echo "[INFO]: Copied ./$genFile to $deck_home_dir/$genFile"
+done
+
+#? Copy frontend
+echo "[TASK]: Copying frontend..."
+scp -r -P $deck_port /dist deck@$deck_ip:$deck_home_dir/dist
+echo "[INFO]: Copied ./dist to $deck_home_dir/dist"
+
+#? Copy default files
+echo "[TASK]: Copying defaults..."
+files=($(ls ./defaults))
+
+for file in "${files[@]}"; do
+  if [ -d $file ]; then
+    scp -r -P $deck_port ./defaults/$file deck@$deck_ip:$deck_home_dir/$file
+  else
+    scp -P $deck_port ./defaults/$file deck@$deck_ip:$deck_home_dir/$file
+  fi
+  echo "[INFO]: Copied ./defaults/$file to $deck_home_dir/$file"
+done
 
 echo "[DONE]"
