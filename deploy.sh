20c2ea68fd0737d7d9e754d57118baac97c24d87
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-03-04 17:38:41 -0500

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,31 +15,48 @@ echo "[INFO]: Loaded config"
 echo ""
 echo "[TASK]: Deploying plugin to deck..."
 
+function scpDirRecursive() {
+  # $1 from dir
+  # $2 to dir
+  files=($(ls $1))
+
+  for file in "${files[@]}"; do
+    if [ -d "$1/$file" ]; then
+      scpDirRecursive "$1/$file" "$2/$file"
+    else
+      diff=$(ssh deck@$deck_ip "cat $2/$file" | diff - $1/$file)
+
+      if [ "$diff" != "" ]; then
+        scp -P $deck_port $1/$file deck@$deck_ip:$2/$file
+        echo "[INFO]: Copied $1/$file to $2/$file"
+      else
+        echo "[INFO]: Skipping $1/$file. No changes detected."
+      fi
+    fi
+  done
+}
+
 #? Copy general files
 echo "[TASK]: Copying general files..."
 genFiles=(LICENSE main.py package.json plugin.json README.md)
 
 for genFile in "${genFiles[@]}"; do
-  scp -P $deck_port $genFile deck@$deck_ip:$deck_home_dir/$genFile
-  echo "[INFO]: Copied ./$genFile to $deck_home_dir/$genFile"
+  diff=$(ssh deck@$deck_ip "cat $deck_home_dir/$genFile" | diff - $genFile)
+
+  if [ "$diff" != "" ]; then
+    # scp -P $deck_port $genFile deck@$deck_ip:$deck_home_dir/$genFile
+    echo "[INFO]: Copied ./$genFile to $deck_home_dir/$genFile"
+  else
+    echo "[INFO]: Skipping $genFile. No changes detected."
+  fi
 done
 
 #? Copy frontend
 echo "[TASK]: Copying frontend..."
-scp -r -P $deck_port ./dist deck@$deck_ip:$deck_home_dir
-echo "[INFO]: Copied ./dist to $deck_home_dir"
+scpDirRecursive "./dist" "$deck_home_dir/dist"
 
 #? Copy default files
 echo "[TASK]: Copying defaults..."
-files=($(ls ./defaults))
-
-for file in "${files[@]}"; do
-  if [ -d $file ]; then
-    scp -r -P $deck_port ./defaults/$file deck@$deck_ip:$deck_home_dir/
-  else
-    scp -P $deck_port ./defaults/$file deck@$deck_ip:$deck_home_dir/$file
-  fi
-  echo "[INFO]: Copied ./defaults/$file to $deck_home_dir/$file"
-done
+scpDirRecursive "./defaults" "$deck_home_dir"
 
 echo "[DONE]"
