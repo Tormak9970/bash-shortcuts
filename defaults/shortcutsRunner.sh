22f1c8c80611370cbf820b1383141342a90154d7
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-03-04 18:16:59 -0500

    started work on window resize bug

--- defaults/shortcutsRunner.sh
+++ defaults/shortcutsRunner.sh
@@ -2,6 +2,11 @@
 
 eval "$@"
 
+# preppedWindowDimensions=$(xrandr --current | grep -oP '/(?<=(current )).*(?=[\,])/i')
+# IFS=' ' read -r -a windowDimensions <<< $preppedWindowDimensions
+# SCREEN_WIDTH="${windowDimensions[0]}"
+# SCREEN_HEIGHT="${windowDimensions[2]}"
+
 SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
 SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
 
